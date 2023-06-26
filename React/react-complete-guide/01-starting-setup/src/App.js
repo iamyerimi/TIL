b75a6fad3396dd90ebb0b674b58b558c99b1f7c3
Yerim Son <iamyerimi@naver.com>
2023-06-27 02:10:37 +0900

    udemy react update 0627

--- React/react-complete-guide/01-starting-setup/src/App.js
+++ React/react-complete-guide/01-starting-setup/src/App.js
@@ -1,8 +1,53 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
+  // 아래 배열 expenses의 객체들의 데이터를 다른 expenseItem에 전달하고 싶어!
+  // == ExpenseItem을 외부로부터 설정할 수 있도록 하고 싶다는 말.
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <p>This is also visible!</p>
+      <ExpenseItem
+        title={expenses[0].title} // 점 뒤의 부분은 프로퍼티의 이름과 일치해야 한다. (이 객체들에 접근하고 있기 때문.)
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      />
     </div>
   );
 }
