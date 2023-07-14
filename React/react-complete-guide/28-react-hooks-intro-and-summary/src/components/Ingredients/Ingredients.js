b66164ee0b1a262ce511277554170d9242d37e5a
Yerim Son <iamyerimi@naver.com>
2023-07-14 15:25:28 +0900

    0712-0713

--- React/react-complete-guide/28-react-hooks-intro-and-summary/src/components/Ingredients/Ingredients.js
+++ React/react-complete-guide/28-react-hooks-intro-and-summary/src/components/Ingredients/Ingredients.js
@@ -1,19 +1,138 @@
-import React from 'react';
+import React, { useCallback, useEffect, useReducer, useState } from "react";
 
-import IngredientForm from './IngredientForm';
-import Search from './Search';
+import IngredientForm from "./IngredientForm";
+import Search from "./Search";
+import IngredientList from "./IngredientList";
+import ErrorModal from "../UI/ErrorModal";
 
-function Ingredients() {
+// 리듀서 정의: 컴포넌트 밖. 컴포넌트가 렌더링 될 때마다 리듀서 함수가 다시 생성되지 않게.
+const ingredientReducer = (currentIngredients, action) => {
+  switch (action.type) {
+    case "SET":
+      return action.ingredients;
+    case "ADD":
+      return [...currentIngredients, action.ingredient];
+    case "DELETE":
+      return currentIngredients.filter((ing) => ing.id !== action.id);
+    default:
+      throw new Error("Should not get there! ");
+  }
+};
+
+const Ingredients = () => {
+  const [userIngredients, dispatch] = useReducer(ingredientReducer, []);
+
+  // const [userIngredients, setUserIngredients] = useState([]);
+  //로딩 스피너 만들기
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
+  // Search 컴포넌트에서 이미 ingredients를 fetching하고 있으니깐 삭제고
+  // useEffect(() => {
+  //   fetch(
+  //     "https://react-hooks-update-2e63e-default-rtdb.firebaseio.com/ingredients.json"
+  //   )
+  //     .then((response) => response.json())
+  //     .then((responseData) => {
+  //       const loadedIngredients = [];
+  //       for (const key in responseData) {
+  //         loadedIngredients.push({
+  //           id: key,
+  //           title: responseData[key].title,
+  //           amount: responseData[key].amount,
+  //         });
+  //       }
+  //       setUserIngredients(loadedIngredients);
+  //     });
+  // }, []);
+
+  useEffect(() => {
+    console.log("RENDERING INGREDIENTS", userIngredients);
+  });
+
+  // 검색핸들러
+  const filteredIngredientsHandler = useCallback((filteredIngredients) => {
+    // setUserIngredients(filteredIngredients);
+    // dispatch로 userIngredients를 업데이트 하도록 변경
+    dispatch({type: 'SET', ingredients: filteredIngredients})
+  }, []);
+
+  const addIngredientHandler = (ingredient) => {
+    setIsLoading(true);
+    fetch(
+      "https://react-hooks-update-2e63e-default-rtdb.firebaseio.com/ingredients.json",
+      {
+        method: "POST", //POST 방식 지정
+        body: JSON.stringify(ingredient),
+        headers: { "Content-Type": "application/json" },
+      }
+    )
+      .then((response) => {
+        setIsLoading(false);
+        return response.json();
+      })
+      .then((responseData) => {
+        // 첫번째 then에서 본문을 가져온 뒤 실행되는 두 번째 .then()
+        // 응답 데이터 처리
+        console.log(ingredient);
+        // then에 넘겨준 함수는 요청이 완료된 뒤에 실행됨. 따라서 요청 완료 후 재료 목록 업데이트함.
+        
+        // setUserIngredients((prevIngredients) => [
+        //   { id: responseData.name, ...ingredient }, // responseData.name: firebase에서 자동으로 생성한 id.
+        //   ...prevIngredients,
+        // ]);
+
+        dispatch({type: 'ADD', ingredient: { id: responseData.name, ...ingredient }})
+      });
+  };
+
+  // ingredient 삭제
+  const removeIngredientHandler = (ingredientId) => {
+    setIsLoading(true);
+    fetch(
+      `https://react-hooks-update-2e63e-default-rtdb.firebaseio.com/ingredients/${ingredientId}.json`,
+      {
+        method: "DELETE",
+      }
+    )
+      .then((response) => {
+        setIsLoading(false);
+        console.log(ingredientId); //이게 선택한 id값
+        // setUserIngredients((prevIngredients) =>
+          // prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
+        // );
+        dispatch({type: 'DELETE', id: ingredientId})
+      })
+      .catch((error) => {
+        // fetch는 promise를 반환하기 때문에 에러 처리는 catch()에서...
+        setError(error.message); //모든 Error객체에는 message 프로퍼티가 있음.
+        // setError("Something went wrong!!")
+        setIsLoading(false);
+
+        //위의 두 state가 동시에 업데이트되고 렌더링 됨. 각각 따로 되는 게 아님!
+      });
+  };
+
+  // 에러 처리
+  const clearError = () => {
+    setError(null);
+  };
   return (
     <div className="App">
-      <IngredientForm />
-
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+      <IngredientForm
+        onAddIngredient={addIngredientHandler}
+        loading={isLoading}
+      />
       <section>
-        <Search />
-        {/* Need to add list here! */}
+        <Search onLoadIngredients={filteredIngredientsHandler} />
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={removeIngredientHandler}
+        />
       </section>
     </div>
   );
-}
+};
 
 export default Ingredients;
--- React/react-complete-guide/28-react-hooks-intro-and-summary/src/components/Ingredients/Search.js
+++ React/react-complete-guide/28-react-hooks-intro-and-summary/src/components/Ingredients/Search.js
@@ -1,15 +1,62 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from "react";
 
-import Card from '../UI/Card';
-import './Search.css';
+import Card from "../UI/Card";
+import "./Search.css";
 
-const Search = React.memo(props => {
+const Search = React.memo((props) => {
+  const { onLoadIngredients } = props; // 객체 구조 분해 할당. {객체에 있는 key이름 명시} = 구조 분해or프로퍼티 가져올 객체
+  // 해당 키를 객체에서 꺼내 onLoadIngredients라는 상수에 저장함(상수로 분리됨).
+  // 이제 onLoadIngredients로 호출될 수 있고, onLoadIngredients를 의존성으로 지정할 수 있음
+  const [enteredFilter, setEnteredFilter] = useState(""); // 배열 구조 분해 할당
+  const inputRef = useRef();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      //아래의 enteredFilter는 500밀리초 전에 입력된 값.(현재 입력된 값 X), useEffect가 실행된 시점.
+      if (enteredFilter === inputRef.current.value) {
+        //current는 ref객체가 가진 프로퍼티임.(여기서는 input을 의미)
+        // inputRef.current.value는 inputRef가 클로저 바깥에 정의되어 있기 때문에 현재 input요소의 value값이다.
+
+        // firebase의 filter 쓰기: 매개변수 query 붙이기.(=> query변수 만들어서 저장 후 url뒤에 붙임)
+        const query =
+          enteredFilter.length === 0
+            ? ""
+            : `?orderBy="title"&equalTo="${enteredFilter}"`;
+        fetch(
+          "https://react-hooks-update-2e63e-default-rtdb.firebaseio.com/ingredients.json" +
+            query
+        )
+          .then((response) => response.json())
+          .then((responseData) => {
+            const loadedIngredients = [];
+            for (const key in responseData) {
+              loadedIngredients.push({
+                id: key,
+                title: responseData[key].title,
+                amount: responseData[key].amount,
+              });
+            }
+            onLoadIngredients(loadedIngredients);
+          });
+      }
+    }, 500);
+    return () => {
+      clearTimeout(timer);
+    }; // clean up 함수(다음이펙트 실행 전 동작). useEffect함수가 다시 실행되기 직전에 실행됨. 여기에서 이전 타이머를 지울거임.
+  }, [enteredFilter, onLoadIngredients, inputRef]);
+  // inputRef도 이펙트 안에서 쓰고 있기 때문에 의존성 값이다.
+  // 다른 값이나 props가 변경되어도 실행되지 않고, enteredFilter가 변경되거나 onLoadIngredients함수가 변경될 때만 실행.(그 때만 요청을 전송하도록 코드짬)
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input
+            ref={inputRef}
+            type="text"
+            value={enteredFilter}
+            onChange={(event) => setEnteredFilter(event.target.value)}
+          />
         </div>
       </Card>
     </section>
